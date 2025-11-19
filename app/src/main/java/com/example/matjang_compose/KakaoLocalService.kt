aef01b726f869357c53a642d1f7f5dc33ad08b47
gotlr98 <gotlr98@naver.com>
2025-11-19 14:38:30 +0900

    edit MatjipModel & add MainMapVM & BottomSheet

---
+++ app/src/main/java/com/example/matjang_compose/KakaoLocalService.kt
@@ -0,0 +1,27 @@
+package com.example.matjang_compose
+
+// api/KakaoLocalService.kt
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoLocalService {
+    @GET("v2/local/search/category.json")
+    suspend fun searchByCategory(
+        // "Authorization: KakaoAK ${REST_API_KEY}" 헤더 추가
+        @Header("Authorization") apiKey: String,
+
+        // 검색할 카테고리 그룹 코드 (FD6: 음식점)
+        @Query("category_group_code") categoryGroupCode: String = "FD6",
+
+        // 현재 지도 중심 좌표 (경도 X, 위도 Y)
+        @Query("x") x: Double,
+        @Query("y") y: Double,
+
+        // 검색 반경 (미터 단위)
+        @Query("radius") radius: Int = 2000, // 예시: 반경 2km
+
+        // 정렬 기준 (거리순)
+        @Query("sort") sort: String = "distance"
+    ): MatjipResponse
+}
